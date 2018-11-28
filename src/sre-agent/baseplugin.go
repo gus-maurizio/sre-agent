3df2e90629f680e8d56b0ea44443713fb93ce65b
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-11-28 06:32:20 -0500

    Wed Nov 28 06:32:20 EST 2018

---
+++ src/sre-agent/baseplugin.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"sre-agent/types"
+        "github.com/google/uuid"
+        "github.com/prometheus/client_golang/prometheus"
+	"log"
+	"runtime"
+	"time"
+)
+
+func baseMeasure() string {
+	caller := "not available"
+	whoami := "not available"
+
+	pc, _, _, ok := runtime.Caller(1)
+	details := runtime.FuncForPC(pc)
+	if ok && details != nil {
+		caller = details.Name()
+	}
+
+	me, _, _, ok := runtime.Caller(0)
+	mydetails := runtime.FuncForPC(me)
+	if ok && mydetails != nil {
+		whoami = mydetails.Name()
+	}
+	return (p.Sprintf("sample %20s called by %20s at %f", whoami, caller, float64(time.Now().UnixNano())/1e9))
+}
+
+func pluginMaker(context types.Context, duration time.Duration, pName string, plugin types.FPlugin, measure types.FuncMeasure) {
+	logrecord := p.Sprintf("PluginMaker context [%v] with duration %v name: %s and function %#v with function_measure %#v\n", context, duration, pName, plugin, measure)
+	log.Print(logrecord)
+	pRuntime := types.PluginRuntime{Ticker: time.NewTicker(duration), PluginName: pName}
+	pContext := context
+	PluginSlice = append(PluginSlice, pRuntime)
+	go plugin(pContext, pRuntime.PluginName, pRuntime.Ticker, measure)
+}
+
+func basePlugin(myParentContext types.Context, myName string, ticker *time.Ticker, measure types.FuncMeasure) {
+	// make sure we Stop at end
+	myContext := myParentContext
+        myContext.TraceId  = uuid.New().String()
+	defer ticker.Stop()
+	log.Printf("%s started context: [%v]\n", myName, myContext)
+	for t := range ticker.C {
+		myContext.Timestamp = float64(t.UTC().UnixNano())/1e9
+        	myContext.RequestId = uuid.New().String()
+		myContext.ParentId  = ""
+		myMeasure := measure()
+		log.Printf("%20s Tick at %f measure: [%v] context: [%v]\n", myName, float64(t.UTC().UnixNano())/1e9, myMeasure, myContext)
+		messageMetric.With(prometheus.Labels{"plugin":myName}).Inc()
+		bytesMetric.With(prometheus.Labels{"plugin":myName}).Add(float64(len(myMeasure)))
+	}
+}
+
---
+++ src/sre-agent/collector.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"sre-agent/types"
+	"github.com/prometheus/client_golang/prometheus"
+	"net"
+	"os"
+	"os/user"
+)
+
+//Define the metrics we wish to expose
+var fooMetric = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "agent_foometric",
+	Help: "Shows whether a foo has occurred in our cluster",
+})
+
+var messageMetric = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "agent_plugin_ticks",
+		Help: "Number of times plugin has executed.",
+	},
+	[]string{"plugin"},
+)
+
+var bytesMetric = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "agent_bytes_sent",
+		Help: "Number of bytes plugin has generated.",
+	},
+	[]string{"plugin"},
+)
+
+
+var myContext types.Context 
+
+
+func init() {
+	//Register metrics with prometheus
+	prometheus.MustRegister(fooMetric)
+	prometheus.MustRegister(messageMetric)
+	prometheus.MustRegister(bytesMetric)
+
+	//Set fooMetric to 1
+	fooMetric.Set(0)
+
+	//Get all the components needed to populate Context.
+	osUser, _ := user.Current()
+	myContext.UserId  = osUser.Username 
+	myContext.UserUID = osUser.Uid 
+	myContext.ExecuteId, _ = os.Hostname()
+	myContext.AccountId = "000000000000"
+	// need to get ALL ip addresses from all interfaces
+	ifaces, _ := net.Interfaces()
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		// handle err
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			// process IP address
+			myContext.IPaddress = append(myContext.IPaddress, ip.String())
+		}
+	}
+}
