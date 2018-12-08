13115e86a79294d448481e15275507e65275ae74
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-08 13:45:25 -0500

    Sat Dec  8 13:45:25 EST 2018

--- scripts/buildplugins.bash
+++ scripts/buildplugins.bash
@@ -2,12 +2,15 @@
 plugdir=${1:-plugins}
 plugos=${2:-darwin}
 plugarch=${3:-amd64}
+export GOOS=$plugos
+export GOARCH=$plugarch
+echo go build -o $GOOS/$GOARCH/sreagent github.com/gus-maurizio/sre-agent
+go build -o $GOOS/$GOARCH/sreagent github.com/gus-maurizio/sre-agent
 for i in $(find ${plugdir} -type f -name 'plugin_*.go')
 do
     echo compiling $i
     object=$(echo $i | sed 's/.go/.so/')
-    GOOS=$plugos
-    GOARCH=$plugarch
     echo go build -buildmode=plugin -o $GOOS/$GOARCH/$object $i
-    go build -buildmode=plugin -o $GOOS/$GOARCH/$object $i
+    CGO_ENABLED=0 go build -buildmode=plugin -o $GOOS/$GOARCH/$object $i
 done
+find $plugos -type f | xargs file
