d1f1da90717df0ae24b8eecede5613c05f9c3ec6
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-09 17:52:41 -0500

    Sun Dec  9 17:52:41 EST 2018

--- scripts/build.bash
+++ scripts/build.bash
@@ -2,14 +2,14 @@
 plugdir=${1:-plugins}
 plugos=${2:-darwin}
 plugarch=${3:-amd64}
-echo go build -o $plugos/plugarch/sreagent github.com/gus-maurizio/sre-agent
-go build -o $plugos/plugarch/sreagent github.com/gus-maurizio/sre-agent
+echo go build -o $plugos/$plugarch/sreagent github.com/gus-maurizio/sre-agent
+go build -o $plugos/$plugarch/sreagent github.com/gus-maurizio/sre-agent
 for i in $(find ${plugdir} -type f -name 'plugin_*.go')
 do
     echo compiling $i
     object=$(echo $i | sed 's/.go/.so/')
-    echo go build -buildmode=plugin -o $plugos/plugarch/$object $i
-    go build -buildmode=plugin -o $plugos/plugarch/$object $i
+    echo go build -buildmode=plugin -o $plugos/$plugarch/$object $i
+    go build -buildmode=plugin -o $plugos/$plugarch/$object $i
 done
 find $plugos -type f | xargs file
 find $plugos -type f | xargs -I {} ldd {}
