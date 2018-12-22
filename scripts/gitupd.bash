d34fa3898bf71da9c50c3f14ed38bf28068b6be3
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-22 17:55:37 -0500

    Sat Dec 22 17:55:37 EST 2018

--- scripts/gitupd.bash
+++ scripts/gitupd.bash
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+DDD="$(date +%Y.%j)"
 for i in plug* sre-agent
 do
   echo Updating GIT $i
   cd $i
-  git add -A;git commit -m "$(date)";git push
+  git add -A
+  git commit -m "$(date)"
+  git tag "${1:-0.9.$DDD}"
+  git push --follow-tags
   cd ..
 done
