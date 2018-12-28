0c1ef92f4180e3b645371cacd54f3754e41b3c4e
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-27 21:28:45 -0500

    Thu Dec 27 21:28:45 EST 2018

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-go build -buildmode=plugin -o $GOPATH/src/github.com/gus-maurizio/plugin_mem/$(uname -s)/plugin_mem.so $GOPATH/src/github.com/gus-maurizio/plugin_mem/plugin_mem.go
 repo=github.com/gus-maurizio
-pack="plugin_mem plugin_cpu plugin_disk plugin_filesystem plugin_load plugin_net plugin_connectionsi plugin_ibmmq"
+pack="plugin_mem plugin_cpu plugin_disk plugin_filesystem plugin_load plugin_net plugin_connections plugin_ibmmq"
 packages=${1:-$pack}
 mod="sre-agent"
 mainmod=${2:-$mod}
@@ -12,19 +11,22 @@ MOSTYPE=$(uname -s)
 
 echo Building for $OSTYPE
 
-for i in $packages
-do 
-  echo Building $i 
-  echo go build -buildmode=plugin -o $GOPATH/src/$repo/$i/$OSTYPE/$i.so $GOPATH/src/$repo/$i/$i.go 
-  go build -buildmode=plugin -o $GOPATH/src/$repo/$i/$OSTYPE/$i.so $GOPATH/src/$repo/$i/$i.go 
-done
 for i in $mainmod
 do
-  echo Building $i
+  echo Building $i for $OSTYPE and OS $MOSTYPE 
   echo go build -o $GOPATH/src/$repo/$i/$MOSTYPE/$i $GOPATH/src/$repo/$i
   go build -o $GOPATH/src/$repo/$i/$MOSTYPE/$i $GOPATH/src/$repo/$i
+  file $GOPATH/src/$repo/$i/$MOSTYPE/$i
 done
-find $GOPATH/src/github.com/gus-maurizio/*/$OSTYPE -type f|xargs file
-[ "$(uname -s)" == "Darwin" ] && find $GOPATH/src/github.com/gus-maurizio/*/$OSTYPE -type f | xargs -I {} otool -L {}
-[ "$(uname -s)" == "Linux"  ] && find $GOPATH/src/github.com/gus-maurizio/*/$OSTYPE -type f | xargs -I {} ldd {}
+[ "$(uname -s)" == "Linux" ] && [ "$MOSTYPE" == "Alpine" ] && echo "Done with $MOSTYPE" && exit 0
+
+for i in $packages
+do 
+  echo Building $i for $OSTYPE and OS $MOSTYPE 
+  echo go build -buildmode=plugin -o $GOPATH/src/$repo/$i/$OSTYPE/$i.so $GOPATH/src/$repo/$i/$i.go 
+  go build -buildmode=plugin -o $GOPATH/src/$repo/$i/$OSTYPE/$i.so $GOPATH/src/$repo/$i/$i.go 
+  file $GOPATH/src/$repo/$i/$OSTYPE/$i.so
+done
+[ "$(uname -s)" == "Darwin" ] && find $GOPATH/src/github.com/gus-maurizio/*/$OSTYPE  -type f | xargs -I {} otool -L {}
+[ "$(uname -s)" == "Linux"  ] && find $GOPATH/src/github.com/gus-maurizio/*/$OSTYPE  -type f | xargs -I {} ldd {}
 [ "$(uname -s)" == "Linux"  ] && find $GOPATH/src/github.com/gus-maurizio/*/$MOSTYPE -type f | xargs -I {} ldd {}
--- scripts/xcompile.bash
+++ scripts/xcompile.bash
@@ -2,10 +2,11 @@
 docker pull gmaurizio/go-ubuntu:1.11.4 || exit 4
 docker pull gmaurizio/go-alpine:latest || exit 8
 
-docker run --rm -it -v $GOPATH:/mnt --name goubuntu gmaurizio/go-ubuntu:1.11.4 \
+docker run --rm -it -v $GOPATH:/mnt --name goalpine gmaurizio/go-alpine:latest \
 /bin/bash -c 'cd /mnt/src/github.com/gus-maurizio/ && sre-agent/scripts/build.bash '
 
-docker run --rm -it -v $GOPATH:/mnt --name goalpine gmaurizio/go-alpine:latest \
+
+docker run --rm -it -v $GOPATH:/mnt --name goubuntu gmaurizio/go-ubuntu:1.11.4 \
 /bin/bash -c 'cd /mnt/src/github.com/gus-maurizio/ && sre-agent/scripts/build.bash '
 
 
